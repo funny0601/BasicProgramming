3c61cd6d3a3bc08df0d8fab0d0532b09f8418000
Seyoung Choi <tpdude0601@gmail.com>
2019-02-25 04:35:00 +0900

    Add files via upload

---
+++ C/프기초 과제 3.c
@@ -0,0 +1,184 @@
+﻿//방사능 물질이 1/10이하로 줄어들 때까지 걸리는 시간 계산
+#include <stdio.h>
+
+int main(void)
+{
+	int halflife;
+	double initial;
+	double current;
+	int years = 0;
+
+	printf("반감기를 입력하시오(년): ");
+	scanf("%d", &halflife);
+
+	initial = 100.0;
+	current = initial;
+	while (current > initial / 10.0)
+	{
+		years += halflife;
+		current = current / 2.0;
+		printf("%d년 후에 남은 양=%f\n", years, current);
+	}
+	printf("1/10 이하로 되기까지 걸린 시간=%d년\n", years);
+	return 0;
+}
+//숫자 추측 프로그램
+#include <stdio.h>
+
+int main(void)
+{
+	int answer = 59;
+	int guess;
+	int tries = 0;
+
+	do{
+		printf("정답을 추측하여 보시오: ");
+		scanf("%d", &guess);
+		tries++;
+
+		if (guess > answer)
+			printf("제시한 정수가 높습니다. \n");
+		if (guess < answer)
+			printf("제시한 정수가 낮습니다. \n");
+	} while (guess != answer);
+
+	printf("축하합니다. 시도횟수=%d\n", tries);
+	return 0;
+}
+//숫자 추측 프로그램
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	int guess, number;
+	int tries = 0;
+
+	srand((unsigned)time(NULL));
+	number = rand() % 101;
+	do{
+		printf("정답을 추측하여 보시오: ");
+		scanf("%d", &guess);
+		tries++;
+
+		if (guess > number)
+			printf("제시한 정수가 높습니다. \n");
+		if (guess < number)
+			printf("제시한 정수가 낮습니다. \n");
+	} while (guess != number);
+
+	printf("축하합니다. 시도횟수=%d\n", tries);
+	printf("%d\n", number);
+	return 0;
+}
+
+//직각 삼각형 찾기
+
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 1; x <= 100; x++)
+	{
+		for (y = 1; y <= 100; y++)
+		{
+			for (z = 1; z <= 100; z++)
+			{
+				if ((x*x + y*y) == z*z)
+					printf("%d %d %d\n", x, y, z);
+			}
+		}
+	}
+	return 0;
+}
+
+
+
+//1부터 100사이의 모든 3의 배수의 합
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int sum = 0;
+
+	for (n = 1; n < 100; n++)
+		if (n % 3 == 0)
+			sum = sum + n;
+	printf("1부터 100사이의 모든 3의 배수의 합은 %d입니다.", sum);
+
+	return 0;
+}
+//1부터 100까지 3의 배수의 합
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 1;
+	int sum = 0;
+	
+	do
+	{
+		if (n % 3 == 0)
+			sum += n;
+		n++;
+	} while (n <= 100);
+
+
+	printf("1부터 100사이의 모든 3의 배수의 합은 %d입니다.", sum);
+	return 0;
+}
+
+
+//패턴 출력 프로그램
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 1; x <= 7; x++)
+	{
+		for (z = 1; z<=7-x; z++)
+			printf(" ");
+	
+		for (y = 0; y < x; y++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	return 0;
+}
+
+//중첩 for루프를 사용하여 출력
+
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y, z;
+	for (x = 1; x <= 5; x++)
+	{
+		for (y = 1; y <= 5 - x; y++)
+			printf(" ");
+		for (z = 1; z <= 2 * x - 1;z++)
+			printf("*"); 
+		printf("\n");
+	}
+
+	for (x = 4; x >= 1; x--)
+	{
+		for (y = 1; y <= 5-x; y++)
+			printf(" ");
+		for (z = 1; z <= 2*x-1; z++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
+
---
+++ C/프기초 과제 4.c
@@ -0,0 +1,182 @@
+﻿#include <stdio.h>
+#define SIZE 10
+
+int main(void)
+{
+	char ans1;
+	int ans2,  i;
+	int seats[SIZE] = {  0 };
+
+	while (1)
+	{
+		printf("좌석을 예약하시겠습니까? (y 또는 n) ");
+		scanf(" %c", &ans1);
+		if (ans1 == 'y')
+		{
+			printf("-------------------------------\n");
+			printf("  1  2  3  4  5  6  7  8  9  10\n");
+			printf("-------------------------------\n");
+
+			for (i = 0; i < SIZE; i++)
+				printf("  %d", seats[i]);
+			printf("\n");
+
+			printf("몇번째 좌석을 예약하시겠습니까? ");
+			scanf("%d", &ans2);
+			if (ans2  <= 0 || ans2> SIZE){
+				printf("1부터 10사이의 숫자를 입력하세요\n");
+				continue;
+			}
+			if (seats[ans2 - 1] == 0 )
+			{
+				seats[ans2 - 1] = 1;
+				printf("예약되었습니다.\n");
+
+			}
+			else
+				printf("이미 예약된 자리입니다. 다른 좌석을 선택하세요. \n");
+
+		}
+		else if (ans1 == 'n')
+			return 0;
+
+	}
+	return 0;
+}
+
+#include <stdio.h>
+#define SIZE 11
+
+int main(void)
+{
+	int freq[SIZE] = { 0 };
+	int i, candidate;
+
+	while (1)
+	{
+		printf("몇번 후보자를 선택하시겠습니까?(종료-1): ");
+		scanf("%d", &candidate);
+		if (candidate < 0) break;
+		freq[candidate]++;
+	}
+
+	printf("값 득표결과\n");
+
+	for (i = 1; i < SIZE; i++)
+		printf("%3d	%3d \n", i, freq[i]);
+
+	return 0;
+}
+
+#include <stdio.h>
+#define SIZE 10
+
+int main(void)
+{
+	char ans1;
+	int ans2, ans3, i;
+	int seats[SIZE] = { 0, 0, 0 };
+
+	while (1)
+	{
+		printf("좌석을 예약하시겠습니까? (y 또는 n) ");
+		scanf(" %c", &ans1);
+		if (ans1 == 'y')
+		{
+			printf("-------------------------------\n");
+			printf("  1  2  3  4  5  6  7  8  9  10\n");
+			printf("-------------------------------\n");
+
+			for (i = 0; i < SIZE; i++)
+				printf("  %d", seats[i]);
+			printf("\n");
+
+			printf("몇번째 좌석을 예약하시겠습니까? (좌석 2개를 골라주세요.)");
+			scanf("%d,%d", &ans2, &ans3);
+			if ((ans2 || ans3) <= 0 || (ans2 || ans3) > SIZE){
+				printf("1부터 10사이의 숫자를 입력하세요\n");
+				continue;
+			}
+			if (seats[ans2 - 1] == 0 && seats[ans3 - 1] == 0)
+			{
+				seats[ans2 - 1] = 1;
+				seats[ans3 - 1] = 1;
+				printf("모든 좌석이 예약되었습니다.\n");
+
+			}
+			else
+				printf("이미 예약된 자리입니다. 다른 좌석을 선택하세요. \n");
+
+		}
+		else if (ans1 == 'n')
+			return 0;
+
+	}
+	return 0;
+}
+
+#include <stdio.h>
+#define SIZE 10
+
+int main(void)
+{
+	int freq[SIZE+1] = { 0 };
+	int i, candidate;
+
+	while (1)
+	{
+		printf("몇번 후보자를 선택하시겠습니까?(종료-1): ");
+		scanf("%d", &candidate);
+		if (candidate < 0) break;
+		freq[candidate]++;
+	}
+
+	printf("값 득표결과\n");
+
+	for (i = 0; i < 10; i++)
+		printf("%3d	%3d \n", i+1, freq[i+1]);
+
+	return 0;
+}
+
+
+
+//매달 요일 얼마까지 있는 지 표시하는 프로그램
+#include <stdio.h>
+
+int main(void)
+{
+	int days[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int i;
+	for (i = 0; i < 12; i++)
+		printf("%d월은 %d일까지 있습니다.\n", i + 1, days[i]);
+	return 0;
+}
+
+
+//최대값과 최소값을 출력하는 프로그램
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int value[10];
+	int i, MAX, MIN;
+
+	
+	for (i = 0; i < 10; i++){
+		value[i] = rand() % 10;
+	}	
+	MAX = value[0];
+	MIN = value[0];
+	for (i = 1; i < 10; i++)
+		MAX = (MAX > value[i]) ? MAX : value[i];
+	printf("최대값: %d\n", MAX);
+	for (int j = 1; j < 10; j++){
+		MIN = (MIN > value[j]) ? value[j] : MIN;
+	}
+		printf("최소값: %d ", MIN);
+	
+	return 0;
+}
+
+
