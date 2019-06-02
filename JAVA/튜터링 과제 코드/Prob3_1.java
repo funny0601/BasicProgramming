881a9f7275c69d674f600918cdefb1629664e902
Seyoung Choi <tpdude0601@gmail.com>
2019-06-02 23:43:18 +0900

    Add files via upload

---
+++ JAVA/튜터링 과제 코드/Prob3_1.java
@@ -0,0 +1,54 @@
+package tutoring;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * 
+ * - 원 2개를 입력받아서 정의한다.
+
+- equals()메소드는 반지름이 같으면 같은 것으로 수정한다.
+ * 
+ * 
+ */
+
+
+class Circle{
+	private int x;
+	private int y;
+	private int radius;
+	
+	public Circle(int x, int y, int radius) {
+		this.x = x;
+		this.y = y;
+		this.radius= radius;
+	}
+	
+	public void equals(Circle c) {
+		if(this.radius==c.radius) {
+			System.out.println("They are the same.");
+		}
+		else
+			System.out.println("They are different.");
+	}
+	
+	public String toString() {
+		return "Circle("+x+","+y+")반지름"+radius;
+	}
+}
+public class Prob3_1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("The first circle (x, y, radius)");
+		Circle a = new Circle(sc.nextInt(), sc.nextInt(), sc.nextInt());
+		System.out.print("The second circle (x, y, radius)");
+		Circle b = new Circle(sc.nextInt(), sc.nextInt(), sc.nextInt());
+		
+		System.out.println("원 a : "+a);
+		System.out.println("원 b : "+b);
+		
+		a.equals(b);
+		
+		sc.close();
+	}
+}
